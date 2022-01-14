9d9160ecbdee7d450c4ada9d99d384f1dcdbd8c2
osamamishmish <osama.mishmish16@gmail.com>
2022-01-14 20:17:00 +0200

    ADD files of the project

---
+++ js/exam.js
@@ -0,0 +1,46 @@
+var questionContent = [
+    {
+        start :document.querySelector(".start"),
+    },
+    {
+        question: "What is the capital of Egypt?",
+        answers: ["Alex", "Aswan", "Cairo"],
+        rightAnswer: 2,
+        next :document.querySelector(".next"),
+    },
+
+    {
+        question: "What is the nationality of Zewail?",
+        answers: ["Egyptian", "American", "England"],
+        rightAnswer: 0,
+        next :document.querySelector(".next"),
+    },
+
+    {
+        question: "Egypt belong to?",
+        answers: ["Africa", "Asia", "Europe"],
+        rightAnswer: 0,
+        next :document.querySelector(".next"),
+        
+    },
+    {
+        question: "when was Al Ahly SC established?",
+        answers: ["1910", "1907", "1903"],
+        rightAnswer: 1,
+        next :document.querySelector(".next"),
+       
+    },
+    {
+        question: "The last time Egypt won the cup of Africa?",
+        answers: ["2012", "2008", "2010"],
+        rightAnswer: 2,
+        next :document.querySelector(".next"),
+        
+    },
+    {
+    result:document.querySelector(".result"),
+    },
+   
+    
+];
+
