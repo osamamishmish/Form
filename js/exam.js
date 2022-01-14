3e319d036b3a7a72ea4657b595564ce676c36066
osamamishmish <osama.mishmish16@gmail.com>
2022-01-14 21:44:57 +0200

    add next button

--- js/exam.js
+++ js/exam.js
@@ -1,46 +1,71 @@
 var questionContent = [
-    {
-        start :document.querySelector(".start"),
-    },
+   
     {
         question: "What is the capital of Egypt?",
         answers: ["Alex", "Aswan", "Cairo"],
         rightAnswer: 2,
-        next :document.querySelector(".next"),
+        next :document.querySelector(".start"),
+        number: "One"
     },
 
     {
         question: "What is the nationality of Zewail?",
         answers: ["Egyptian", "American", "England"],
         rightAnswer: 0,
-        next :document.querySelector(".next"),
+        next :$(".next"),
+        number: "Two"
     },
 
     {
         question: "Egypt belong to?",
         answers: ["Africa", "Asia", "Europe"],
         rightAnswer: 0,
-        next :document.querySelector(".next"),
+        next :document.createElement("button"),
+        number: "Three"
         
     },
     {
         question: "when was Al Ahly SC established?",
         answers: ["1910", "1907", "1903"],
         rightAnswer: 1,
-        next :document.querySelector(".next"),
+        next :document.createElement("button"),
+        number: "Four"
        
     },
     {
         question: "The last time Egypt won the cup of Africa?",
         answers: ["2012", "2008", "2010"],
         rightAnswer: 2,
-        next :document.querySelector(".next"),
+        next :document.createElement("button"),
+        number: "Five"
         
     },
-    {
-    result:document.querySelector(".result"),
-    },
    
+    {
+        next:document.querySelector(".result"),
+        },
     
 ];
 
+$(document).ready(function(){
+    for (let index=0 ;index<questionContent.length-1 ; index++){
+        
+        $(questionContent[index].next).on("click",function(){
+            "use strict";
+            $(questionContent[index].next).addClass("hide");
+                
+               $(questionContent[index+1].next).appendTo(".card-body");
+               $(questionContent[index+1].next).addClass("btn btn-primary").text("next");
+               
+               
+            $(".content").removeClass("hide");
+           // questionContent[0].next.classList.add("hide");
+            $(".card-header").text("Question"+questionContent[index].number);
+            $(".card-title").text(questionContent[index].question);
+            for(let x=0 ;x<questionContent[index].answers.length;x++){
+                $(".answer").eq(x).text(questionContent[index].answers[x]);
+            }  
+        })
+    
+    }
+})
