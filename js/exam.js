6e12efa7163c055b05ac62562b07fb0ee25bf7e2
osamamishmish <osama.mishmish16@gmail.com>
2022-01-16 03:28:01 +0200

    modify js file

--- js/exam.js
+++ js/exam.js
@@ -1,19 +1,21 @@
 var questionContent = [
-   
+  
     {
         question: "What is the capital of Egypt?",
         answers: ["Alex", "Aswan", "Cairo"],
         rightAnswer: 2,
         next :document.querySelector(".start"),
-        number: "One"
+    //   next:$(".next"),
+        number: "One",
     },
 
     {
         question: "What is the nationality of Zewail?",
         answers: ["Egyptian", "American", "England"],
         rightAnswer: 0,
-        next :$(".next"),
-        number: "Two"
+        next :document.createElement("button"),
+        previous:$(".previous"),
+        number: "Two",
     },
 
     {
@@ -21,7 +23,8 @@ var questionContent = [
         answers: ["Africa", "Asia", "Europe"],
         rightAnswer: 0,
         next :document.createElement("button"),
-        number: "Three"
+        previous:document.createElement("button"),
+        number: "Three",
         
     },
     {
@@ -29,7 +32,8 @@ var questionContent = [
         answers: ["1910", "1907", "1903"],
         rightAnswer: 1,
         next :document.createElement("button"),
-        number: "Four"
+        previous:document.createElement("button"),
+        number: "Four",
        
     },
     {
@@ -37,35 +41,120 @@ var questionContent = [
         answers: ["2012", "2008", "2010"],
         rightAnswer: 2,
         next :document.createElement("button"),
-        number: "Five"
+        previous:document.createElement("button"),
+        number: "Five",
         
     },
-   
     {
-        next:document.querySelector(".result"),
-        },
+      //  next :document.createElement("button"),
+        previous:document.createElement("button")
+    },
+   
     
 ];
-
-$(document).ready(function(){
-    for (let index=0 ;index<questionContent.length-1 ; index++){
+//Next
+/*$(document).ready(function(){
+    for (let index=0 ;index<questionContent.length ; index++){
         
         $(questionContent[index].next).on("click",function(){
             "use strict";
             $(questionContent[index].next).addClass("hide");
-                
-               $(questionContent[index+1].next).appendTo(".card-body");
-               $(questionContent[index+1].next).addClass("btn btn-primary").text("next");
-               
-               
+                $(questionContent[index].previous).removeClass("hide");
+               /* $(questionContent[index].previous).addClass("hide");
+                $(questionContent[index+1].previous).appendTo(".card-body");
+                $(questionContent[index+1].previous).addClass("btn btn-secondary").text("previous"); */
+            /*   $(questionContent[index+1].next).appendTo(".card-body");
+               $(questionContent[index+1].next).addClass("btn btn-primary").text("next"); 
+               $(".previous").off("click").on("click",function(){
+                $(".card-header").text("Question"+" "+questionContent[index].number);
+                $(".card-title").text(questionContent[index].question);
+        
+              })
             $(".content").removeClass("hide");
-           // questionContent[0].next.classList.add("hide");
-            $(".card-header").text("Question"+questionContent[index].number);
+            $(".card-header").text("Question"+" "+questionContent[index].number);
             $(".card-title").text(questionContent[index].question);
+
             for(let x=0 ;x<questionContent[index].answers.length;x++){
                 $(".answer").eq(x).text(questionContent[index].answers[x]);
             }  
         })
     
     }
-})
+})*/
+
+
+
+//previous
+/*$(document).ready(function(){
+    for(let index=questionContent.length-1;index>=1;index--){
+        $(questionContent[index].previous).on("click",function(){
+            $(questionContent[index].previous).addClass("hide");
+            $(questionContent[index-1].previous).appendTo(".card-body");
+            $(questionContent[index-1].previous).addClass("btn btn-primary").text("previous");
+            $(".card-header").text("Question"+" "+questionContent[index-1].number);
+            $(".card-title").text(questionContent[index-1].question);
+            for(let x=0 ;x<questionContent[index].answers.length;x++){
+                $(".answer").eq(x).text(questionContent[index-1].answers[x]);
+            }  
+
+        })
+    }
+})*/
+//var theNext=document.createElement("button");
+$(document).ready(function(){
+    for (let index=0;index<questionContent.length; index++){
+        $(questionContent[index].next).on("click",function(){
+            $(".content").removeClass("hide");
+            $(questionContent[index].next).addClass("hide");
+            $(questionContent[index].previous).addClass("hide");
+          //  $(questionContent[index].next).addClass("hide");
+           // $(questionContent[index+1].next).remove();
+            $(questionContent[index+1].next).appendTo(".card-body").attr("type","button").addClass("btn btn-primary next").text("Next");
+   
+            
+              //  $(questionContent[index+1].previous).remove();
+              //  $(".card-header").text("Question"+" "+questionContent[index-1].number);
+                //$(questionContent[index].previous).remove();
+               // $(questionContent[index].next).appendTo(".card-body").attr("type","button").addClass("btn btn-primary next").text("Next");
+          /*  $(".card-title").text(questionContent[index-1].question);
+            for(let y=0 ;y<questionContent[index+1].answers.length;y++){
+                $(".answer").eq(y).text(questionContent[index-1].answers[y]);
+            } */
+          //  });
+            
+            $(".card-header").text("Question"+" "+questionContent[index].number);
+            $(".card-title").text(questionContent[index].question);
+            if($(".card-header").text()=="Question Two"){
+                $(questionContent[1].previous).removeClass("hide");
+            }
+           
+           // $(questionContent[questionContent.length-1].next).appendTo(".card-body").attr("type","button").remove();
+            for(let x=0 ;x<questionContent[index].answers.length;x++){
+                $(".answer").eq(x).text(questionContent[index].answers[x]);
+            } 
+             
+        });
+    }
+});
+
+/*$(document).ready(function(){
+    for (let i=1; i<questionContent.length ;){
+        $(questionContent[i].previous).on("click",function(){
+            $(".card-header").text("Question"+" "+questionContent[i-1].number);
+            $(".card-title").text(questionContent[i-1].question);
+            $(questionContent[i-1].next).removeClass("hide");
+            $(questionContent[1].previous).addClass("hide");
+           // $(questionContent[i].previous).appendTo(".card-body").attr("type","button").addClass("btn btn-primary next").text("Previous");
+           // $(questionContent[i+1].previous).addClass("hide");
+         //   $(questionContent[i-1].previous).addClass("hide");
+       /*  if($(".card-header").text()=="Question Two"){
+            $(questionContent[1].previous).appendTo(".card-body").attr("type","button").addClass("btn btn-secondary previous").text("previous");
+        }*/
+        /*    $(questionContent[i-1].previous).appendTo(".card-body").attr("type","button").addClass("btn btn-secondary next").text("previous");
+            for(let x=0 ;x<questionContent[i].answers.length;x++){
+                $(".answer").eq(x).text(questionContent[i-1].answers[x]);
+            } 
+           // $(questionContent[i].previous).removeClass("hide");
+        })
+    }
+})*/
