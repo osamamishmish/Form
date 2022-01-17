a9dc782fb37f13cad06eccc2ee2e390b9a0c1d37
osamamishmish <osama.mishmish16@gmail.com>
2022-01-17 20:05:21 +0200

    Add Previous button

--- js/exam.js
+++ js/exam.js
@@ -4,8 +4,6 @@ var questionContent = [
         question: "What is the capital of Egypt?",
         answers: ["Alex", "Aswan", "Cairo"],
         rightAnswer: 2,
-        next :document.querySelector(".start"),
-    //   next:$(".next"),
         number: "One",
     },
 
@@ -13,8 +11,6 @@ var questionContent = [
         question: "What is the nationality of Zewail?",
         answers: ["Egyptian", "American", "England"],
         rightAnswer: 0,
-        next :document.createElement("button"),
-        previous:$(".previous"),
         number: "Two",
     },
 
@@ -22,8 +18,6 @@ var questionContent = [
         question: "Egypt belong to?",
         answers: ["Africa", "Asia", "Europe"],
         rightAnswer: 0,
-        next :document.createElement("button"),
-        previous:document.createElement("button"),
         number: "Three",
         
     },
@@ -31,8 +25,6 @@ var questionContent = [
         question: "when was Al Ahly SC established?",
         answers: ["1910", "1907", "1903"],
         rightAnswer: 1,
-        next :document.createElement("button"),
-        previous:document.createElement("button"),
         number: "Four",
        
     },
@@ -40,38 +32,80 @@ var questionContent = [
         question: "The last time Egypt won the cup of Africa?",
         answers: ["2012", "2008", "2010"],
         rightAnswer: 2,
-        next :document.createElement("button"),
-        previous:document.createElement("button"),
         number: "Five",
         
     },
-    {
-      //  next :document.createElement("button"),
-        previous:document.createElement("button")
-    },
+   
    
     
 ];
 
 
 
+
+//start the exam
 $(document).ready(function(){
-    for (let index=0;index<questionContent.length; index++){
-        $(questionContent[index].next).on("click",function(){
+        $(questionContent[0].previous).addClass("hide");
+        $(".start").on("click",function(){
+            $(this).remove();
             $(".content").removeClass("hide");
-            $(questionContent[index].next).addClass("hide");
-            $(questionContent[index].previous).addClass("hide");
-            $(questionContent[index+1].next).appendTo(".card-body").attr("type","button").addClass("btn btn-primary next").text("Next");
-            $(".card-header").text("Question"+" "+questionContent[index].number);
-            $(".card-title").text(questionContent[index].question);
-            if($(".card-header").text()=="Question Two"){
-                $(questionContent[1].previous).removeClass("hide");
-            }
-            for(let x=0 ;x<questionContent[index].answers.length;x++){
-                $(".answer").eq(x).text(questionContent[index].answers[x]);
+            $(".card-header").text("Question"+" "+questionContent[0].number);
+            $(".card-title").text(questionContent[0].question);
+            for(let x=0 ;x<questionContent[0].answers.length;x++){
+                $(".answer").eq(x).text(questionContent[0].answers[x]);
             } 
-             
-        });
-    }
 });
+})
+
+
+
+
+let index=0;
+//Next
+$(document).ready(function(){
+        $(".next").on("click",function(){
+          index++;
+          if(index==1){
+            $(".previous").removeClass("hide");
+          }
+          if(index==questionContent.length-1){
+              $(".next").addClass("hide");
+          }else{
+            $(".next").removeClass("hide");
+          }
+
+$(".card-header").text("Question"+" "+questionContent[index].number);
+$(".card-title").text(questionContent[index].question);
+for(let x=0;x<questionContent[index].answers.length;x++){
+    $(".answer").eq(x).text(questionContent[index].answers[x]);
+} 
+})
+})
+
+
+
+//previous
+$(document).ready(function(){
+    $(".previous").on("click",function(){
+      index--;
+      if(index<questionContent.length-1){
+        $(".next").removeClass("hide");
+      }
+     if(index==0){
+          $(".previous").addClass("hide");
+      }else{
+        $(".previous").removeClass("hide");
+      }
+
+$(".card-header").text("Question"+" "+questionContent[index].number);
+$(".card-title").text(questionContent[index].question);
+for(let x=0;x<questionContent[index].answers.length;x++){
+$(".answer").eq(x).text(questionContent[index].answers[x]);
+} 
+})
+});
+
+
+
+
 
