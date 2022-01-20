f8bcb7e737a4c1b9d7d535cfea74591e6f27c000
osamamishmish <osama.mishmish16@gmail.com>
2022-01-20 21:39:39 +0200

    Delete unused code

--- js/exam.js
+++ js/exam.js
@@ -46,6 +46,7 @@ var questionContent = [
 ];
  var resultArr=[];
 
+ 
 
 
 //start the exam
@@ -53,19 +54,11 @@ $(document).ready(function(){
         $(questionContent[0].previous).addClass("hide");
         $(".start").on("click",function(){
             $(this).remove();
+            $(".the-check").hide();
             $(".content").removeClass("hide");
             $(".card-header").text("Question"+" "+questionContent[0].number);
             $(".card-title").text(questionContent[0].question);
-            for(let index=0 ;index<questionContent[0].answers.length;index++){
-                $(".check").eq(index).on("click",function(){
-                    var theIndex=  questionContent[0].answers.indexOf($(".check").eq(index).siblings().text());
-                      console.log(theIndex);
-                  });
-            }
-            
-            for(let x=0 ;x<questionContent[0].answers.length;x++){
-                
-            
+            for(let x=0 ;x<questionContent[0].answers.length;x++){        
                 $(".answer").eq(x).text(questionContent[0].answers[x]);
                 
             } 
@@ -73,46 +66,16 @@ $(document).ready(function(){
 })
 
 
-/*$(document).ready(function(){
-
-for(let i=0 ;i<questionContent[0].answers.length;i++){
-var theChecked=document.querySelector(".answer-container")[i];
-}
-   // for(let index=0 ;index,questionContent.length;index++){
-   // for(let i=0 ;i<questionContent[0].answers.length;i++){
-        $(theChecked).children().eq(0).on("click",function(){
-            $(this).parent().attr("checked","true");
-            $(this).parent().siblings().children().eq(0).removeAttr("checked");
-
-          //  $(".answer-container").hasAttr("checked").siblings().removeAttr("checked");
-           // $(".check").eq(i+1).attr("checked","false");
-            //$(".check").eq(i-1).attr("checked","false");
-        })
-   // }
-    
-    
-        
-    
-        
-//} 
-})*/
-
 var theContainer=document.querySelectorAll(".answer-container");
 let index=0;
+
+
 //Next
 $(document).ready(function(){
         $(".next").on("click",function(){
+            $(".check").addClass("hide");
+            index++;
            
-          //  $(".the-check").addClass("hide");
-          index++;
-         /* if(index=index++){
-              for (let y=0 ;y<3;y++){
-                var theRadio=document.createElement("input");
-                
-                $(theRadio).eq(y).attr("type","radio").prependTo(".answer-container").addClass("check");
-               // $(".check").addClass("hide");
-              }
-          }*/
           if(index==1){
             $(".previous").removeClass("hide");
           }
@@ -121,25 +84,21 @@ $(document).ready(function(){
           }else{
             $(".next").removeClass("hide");
           }
-
+        
 $(".card-header").text("Question"+" "+questionContent[index].number);
 $(".card-title").text(questionContent[index].question);
-for(let i=0 ;i<questionContent[index].answers.length;i++){
-    $(".check").eq(i).on("click",function(){
-        var theIndex=  questionContent[index].answers.indexOf($(".check").eq(i).siblings().text());
-          console.log(theIndex);
-      });
-}
-
-
-//$(".the-check").addClass("hide");
+for(let i=0 ;i<questionContent.length-1;i++){
+    var theCheckField=document.createElement("input");
+    $(theCheckField).attr("type","radio").addClass("the-check");
+    $(theCheckField).prependTo(".answer-container");
 for(let x=0;x<questionContent[index].answers.length;x++){
     
-    
     $(".answer-container").eq(x).children().last().text(questionContent[index].answers[x]);
-   // $(theContainer).eq(x).children().eq(0).addClass("hide");
-   // $(".answer").eq(x).text(questionContent[index].answers[x]);
-} 
+    
+
+    }
+}
+
 })
 })
 
@@ -149,6 +108,12 @@ for(let x=0;x<questionContent[index].answers.length;x++){
 $(document).ready(function(){
     $(".previous").on("click",function(){
       index--;
+      if(index==0){
+        $(".check").removeClass("hide");
+      }else{
+          $(".check").addClass("hide");
+      }
+    
       if(index<questionContent.length-1){
         $(".next").removeClass("hide");
       }
@@ -157,31 +122,17 @@ $(document).ready(function(){
       }else{
         $(".previous").removeClass("hide");
       }
-      $(".check").attr("name",index);
+   
+$(".check").attr("name",index);
+$(".the-check").attr("name",index);
 $(".card-header").text("Question"+" "+questionContent[index].number);
 $(".card-title").text(questionContent[index].question);
 for(let x=0;x<questionContent[index].answers.length;x++){
+ 
 $(".answer").eq(x).text(questionContent[index].answers[x]);
 } 
 })
 });
-
-
-
-/*var theChecked =document.querySelector(".check"),
-    checkStatus =theChecked.getAttribute("checked");
-$(document).ready(function(){
-    switch($(".check").attr("name")){
-        case 0 :
-            if(checkStatus==true){
-                console.log($(theChecked).next().text());
-            };
-            break;
-            default:
-                console.log("hhhhhhhhh");
-                break;
-    }
-})*/
 
 
 
